4780ccf4e8e0e34d057131edbfcb214211354fdb
Jawad <mjawadhussain40@gmail.com>
2025-12-15 17:21:39 +0530

    new

--- nc11.py
+++ nc11.py
@@ -11,6 +11,23 @@ class Solution:
                 l+=1
             if numbers[l] + numbers[r] > target:
                 r-=1
+        class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+        n = len(numbers)
+        l,r = 0, n-1
 
+        while l < r:
+            if numbers[l] + numbers [r] == target:
+                return [l+1,r+1]
+            if numbers[l] + numbers[r] < target:
+                l+=1
+            if numbers[l] + numbers[r] > target:
+                r-=1
+
+        return []
+
+               
+        
                
         
