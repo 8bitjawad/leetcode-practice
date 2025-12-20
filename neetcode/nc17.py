a9c81842eec97111c162fe7066fbf2d7e291b1d3
Jawad <mjawadhussain40@gmail.com>
2025-12-20 23:01:34 +0530

    new

--- neetcode/nc17.py
+++ neetcode/nc17.py
@@ -1,24 +1,38 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
 
+        # count = {}
+        # l = r = 0
+        # res = 0
+
+        # for r in range(len(s)):
+        #     count[s[r]] = 1 + count.get(s[r],0)
+        #     window_length = r-l+1
+        #     max_freq = max(count.values())
+
+        #     temp = window_length - max_freq
+        #     if temp <= k:
+        #         res = max(res, window_length)
+        #     else:
+        #         while (r-l+1) - max(count.values()) > k:
+        #             count[s[l]] -= 1
+        #             l += 1
+
+        # return res
+
         count = {}
-        l = r = 0
+        l = 0
         res = 0
+        maxf = 0
 
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r],0)
-            window_length = r-l+1
-            max_freq = max(count.values())
+            maxf = max(maxf,count[s[r]])
 
-            temp = window_length - max_freq
-            if temp <= k:
-                res = max(res, window_length)
-            else:
-                while (r-l+1) - max(count.values()) > k:
-                    count[s[l]] -= 1
-                    l += 1
+            while (r-l+1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+            
+            res = max(res,r-l+1)
 
         return res
-            
-
-        
